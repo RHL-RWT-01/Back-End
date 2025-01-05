c2f8fcbdee79d56035bc49a259b5f6c03e499cff
Rahul <rahulrawat4060@gmail.com>
2025-01-05 20:20:44 +0530

    ..

--- express/index.js
+++ express/index.js
@@ -1,18 +1,36 @@
-
 const express = require('express');
-
 const app = express();
 
+app.use(express.json()); 
 app.get('/', (req, res) => {
     res.send('Hello, World!........');
 });
 
-app.get('initial',(req, res) =>{
-    const name = req.query.name;
-    res.send('Initial Page', name);
-})
+const person = {
+    name: "Rahul",
+    age: 20,
+    city: "Indore",
+};
 
-const port = 3001;
+app.get('/initial', (req, res) => {
+    res.send(`Initial Page - Name: ${person.name}`);
+});
+
+app.get('/age', (req, res) => {
+    res.send(`Initial Page - Name: ${person.age}`);
+});
+
+app.post('/update', (req, res) => {
+    const newName = req.body.name; 
+    if (newName) {
+        person.name = newName;
+        res.send('Name updated successfully');
+    } else {
+        res.status(400).send('Name is required');
+    }
+});
+
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
