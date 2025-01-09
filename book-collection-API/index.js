22abb7f1a6829749104aead49aea468efaf54ced
Rahul <rahulrawat4060@gmail.com>
2025-01-09 22:37:35 +0530

    started project book MGT API

---
+++ book-collection-API/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port=3000;
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('Welcome')
+});
+
+app.listen(port);
---
+++ book-collection-API/schemas/categorySchema.js
@@ -0,0 +1,8 @@
+const { z } = require('zod');
+
+const categorySchema = z.object({
+  name: z.string().min(1, 'Category name is required'),
+  description: z.string().optional(),
+});
+
+module.exports = categorySchema;
