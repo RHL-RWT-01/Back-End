d903a0f3eaefe1b9793911fe7d503ca38cc00da7
Rahul <rahulrawat4060@gmail.com>
2025-01-09 23:14:40 +0530

    Refactor API structure to include category and book routes with middleware

--- book-collection-API/index.js
+++ book-collection-API/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const categoryRoutes = require('./routes/categoryRoutes');
+const bookRoutes = require('./routes/bookRoutes');
+const logger=require('./middlewares/logger');
+const errorHandler = require('./middlewares/errorHandler');
 const app = express()
 const port=3000;
 app.use(express.json())
+app.use(logger);
 
-app.get('/',(req,res)=>{
-    res.send('Welcome')
-});
+app.use('/categories',categoryRoutes);
+app.use('/books',bookRoutes);
+
+app.use(errorHandler);
 
 app.listen(port);
