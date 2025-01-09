1c23abe186d1b632ae4de83046a9bb2d23625939
Rahul <rahulrawat4060@gmail.com>
2025-01-09 23:01:52 +0530

    Add category routes with validation middleware

---
+++ book-collection-API/routes/categoryRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.router();
+const validate=require('../middlewares/validate');
+const categorySchema = require('../schemas/categorySchema');
+
+let category=[];
+
+router.post('/',validate(categorySchema),(req,res)=>{
+    const category={id:categories.length+1,...req.body};
+    categories.push(category);
+    res.status(201).json(category);
+});
+
+router.get('/',(req,res)=>{
+    res.json(categories);
+});
+
+module.exports=router;
