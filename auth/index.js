5380f75ead8754e5a64d5c87de9e654327241fef
Rahul <rahulrawat4060@gmail.com>
2025-01-11 21:04:28 +0530

    Implement basic authentication with JWT and user management endpoints

---
+++ auth/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const jwtPassword = "123456";
+
+const app = express();
+
+const ALL_USERS = [
+  {
+    username: "rahul@gmail.com",
+    password: "123",
+    name: "rahul rawat",
+  },
+  {
+    username: "raman@gmail.com",
+    password: "123321",
+    name: "Raman singh",
+  },
+  {
+    username: "abcd@gmail.com",
+    password: "123321",
+    name: "abcd kumar",
+  },
+];
+
+function userExists(username, password) {
+  // write logic to return true or false if this user exists
+  // in ALL_USERS array
+  return ALL_USERS.some((user) => user.username === username && user.password === password);
+}
+
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: "User doesnt exist in our in memory db",
+    });
+  }
+
+  var token = jwt.sign({ username: username }, "shhhhh");
+  return res.json({
+    token,
+  });
+});
+
+app.get("/users", function (req, res) {
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, jwtPassword);
+    const username = decoded.username;
+    // return a list of users other than this username
+    return res.json(ALL_USERS.filter((user) => user.username !== username));
+  } catch (err) {
+    return res.status(403).json({
+      msg: "Invalid token",
+    });
+  }
+});
+
+app.listen(3000)
