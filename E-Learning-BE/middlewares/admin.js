9b2472c5c07674047439a1892b72d7f0d11c79f7
Rahul <rahulrawat4060@gmail.com>
2025-01-15 22:50:40 +0530

    implemented jwt auth for user and admin middlewares

--- E-Learning-BE/middlewares/admin.js
+++ E-Learning-BE/middlewares/admin.js
@@ -1,20 +1,33 @@
+require('dotenv').config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
 const {Admin} =require('../db/index');
-
 function adminMiddleware(req, res, next) {
-    const username = req.headers.username;
-    const password = req.headers.password;
+    const token = req.headers.authorization;
+    token = token.split(' ')[1];
+    const decoded = jwt.verify(token, JWT_SECRET);
 
-    Admin.findOne({
-        username: username,
-        password: password
-    })
-      .then((val)=>{
-        if(val){
-            next();
-        }else{
-            res.status(401).send('admin not found');
-        }
-      })
+    if(decoded.id){
+        next();
+    }else{
+        res.status(401).send('admin not found');
+    }
+    
+
+    // const username = req.headers.username;
+    // const password = req.headers.password;
+
+    // Admin.findOne({
+    //     username: username,
+    //     password: password
+    // })
+    //   .then((val)=>{
+    //     if(val){
+    //         next();
+    //     }else{
+    //         res.status(401).send('admin not found');
+    //     }
+    //   })
 }
 
 module.exports = adminMiddleware;
--- E-Learning-BE/middlewares/user.js
+++ E-Learning-BE/middlewares/user.js
@@ -1,20 +1,31 @@
-const {User} =require('../db');
+require('dotenv').config();
+const JWT_SECRET = process.env.JWT_SECRET;
 
 function userMiddleware(req, res, next) {
-    const username = req.headers.username;
-    const password = req.headers.password;
 
-    User.findOne({
-        username: username,
-        password: password
-    })
-      .then((val)=>{
-        if(val){
-            next();
-        }else{
-            res.status(401).send('User not found');
-        }
-      })
+    const token = req.headers.authorization;
+    token = token.split(' ')[1];
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if(decoded.username){
+        next();
+    }else{
+        res.status(401).send('User not found');
+    }
+
+    // const username = req.headers.username;
+    // const password = req.headers.password;
+
+    // User.findOne({
+    //     username: username,
+    //     password: password
+    // })
+    //   .then((val)=>{
+    //     if(val){
+    //         next();
+    //     }else{
+    //         res.status(401).send('User not found');
+    //     }
+    //   })
 }
 
 module.exports = userMiddleware;
