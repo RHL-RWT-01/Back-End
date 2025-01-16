a06c0e57c3fc5140b9362fdcc5ea08cfd5a63962
Rahul <rahulrawat4060@gmail.com>
2025-01-16 23:59:22 +0530

    feat: enhance todo creation with validation response handling

--- TO-DO/Back-End/index.js
+++ TO-DO/Back-End/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const todoSchema = require('./types');
+const {todoSchema} = require('./types');
 
 const app = express();
 const port = 3000;
@@ -9,7 +9,15 @@ app.use(express.json());
 app.post('/todo',(req,res)=>{
     const todo = req.body;
     const result = todoSchema.parse(todo);
-    
+    if(result.success){
+        res.json({
+            message: 'Todo created successfully',
+        }); 
+    }else{
+        res.status(400).json({
+            message: 'Invalid data',
+        });
+    }
 
 })
 
