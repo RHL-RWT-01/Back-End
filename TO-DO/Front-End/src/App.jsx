a76527105ba6f2e20ac5c0bff2576219ce261076
Rahul <rahulrawat4060@gmail.com>
2025-01-17 23:47:10 +0530

    add CORS middleware and update package dependencies

--- TO-DO/Front-End/src/App.jsx
+++ TO-DO/Front-End/src/App.jsx
@@ -1,22 +1,24 @@
-import { useState, useEffect } from 'react'
-import  Create  from './components/Create'
-import Todos from './components/Todos'
+import { useState, useEffect } from 'react';
+import Create from './components/Create';
+import Todos from './components/Todos';
+
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/todos')
-      .then(res => res.json())
-      .then(data => setTodos(data))
-  }, [])
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((error) => console.error('Failed to fetch:', error));
+  }, []);  // The empty array ensures the effect runs only once when the component mounts
 
   return (
     <>
       <h1>TO-DO</h1>
       <Create />
-      <Todos todos={todos}/>
+      <Todos todos={todos} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
