4201176d767faaeb074953e42f1945755866360e
Rahul <rahulrawat4060@gmail.com>
2025-01-17 22:11:49 +0530

    add Create component for adding todos

---
+++ TO-DO/Front-End/src/components/Create.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <>
+    <input type="text" placeholder="title" />
+    <br/>
+    <br/>
+    <input type="text" placeholder="description" />
+    <br/>
+    <br/>
+    <button>Add Todo</button>
+
+    </>
+  )
+}
+
+export default Create
