44699ad051220daac9da1f8f95a140e83f9c7656
Rahul <rahulrawat4060@gmail.com>
2025-01-18 00:10:20 +0530

    add functionality to handle success and error messages when adding todos

--- TO-DO/Front-End/src/components/Create.jsx
+++ TO-DO/Front-End/src/components/Create.jsx
@@ -1,37 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Create() {
-  const [title, setTitle] =useState('');
-  const [description, setDescription] =useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [message, setMessage] = useState('');  // Added state to handle success or error messages
+
+  const addTodo = async () => {
+    const todo = { title, description };
+    try {
+      const response = await fetch("http://localhost:3000/todo", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todo),
+      });
+
+      if (response.ok) {
+        setMessage("Todo added successfully");
+        setTitle('');  // Resetting input fields
+        setDescription('');
+      } else {
+        const errorData = await response.json();
+        setMessage(`Failed to add todo: ${errorData.message}`);
+      }
+    } catch (error) {
+      setMessage(`An error occurred: ${error.message}`);
+    }
+  };
 
   return (
     <>
-    <input style={{
-        padding:15,
-        margin:12
-    }} type="text" placeholder="title" />
-    <br/>
-    <br/>
-    <input style={{
-        padding:15,
-        margin:12
-    }}
-     type="text" placeholder="description" />
-    <br/>
-    <br/>
-    <button style={{
-        backgroundColor:'purple',
-        color:'white',
-        borderRadius:15,
-        border:0,
-        width:'10%',
-        cursor:'pointer',
-        padding:15,
-        margin:20
-    }} >Add Todo</button>
-
+      <input
+        style={{ padding: 15, margin: 12 }}
+        type="text"
+        placeholder="title"
+        id="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <br />
+      <br />
+      <input
+        style={{ padding: 15, margin: 12 }}
+        type="text"
+        placeholder="description"
+        id="description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <br />
+      <br />
+      <button
+        style={{
+          backgroundColor: 'purple',
+          color: 'white',
+          borderRadius: 15,
+          border: 0,
+          width: '10%',
+          cursor: 'pointer',
+          padding: 15,
+          margin: 20,
+        }}
+        onClick={addTodo}
+      >
+        Add Todo
+      </button>
+      {message && <p>{message}</p>}  {/* Display success or error message */}
     </>
-  )
+  );
 }
 
-export default Create
+export default Create;
