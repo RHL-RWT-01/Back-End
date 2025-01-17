f16883aaa3d4fa045e456230da8e289bddd5920b
Rahul <rahulrawat4060@gmail.com>
2025-01-18 00:28:31 +0530

    add functionality to mark todos as complete

--- TO-DO/Front-End/src/components/Todos.jsx
+++ TO-DO/Front-End/src/components/Todos.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
 function Todos({todos}) {
+   
+    const markAsComplete=({id:_id})=>{
+        // mark the todo as complete
+        fetch('http://localhost:3000/completed', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                _id: id,
+            })    
+    })
+    }
   return (
     <>
     <h1>Todo List</h1>
     {todos.map((todo, index) => 
-      <div style={{
+      <div id={todo._id} 
+      style={{
         border: '1px solid black',
         marginBottom: '10px',
         padding: '10px',
@@ -16,7 +30,9 @@ function Todos({todos}) {
         <p>{todo.description}</p>
         <button style={{
             backgroundColor: 'blue',
-        }}>{todo.completed==true?"Comleted":"Mark as Complete"}</button>
+        }}
+        onClick={markAsComplete((todo._id))} 
+        >{todo.completed==true?"Comleted":"Mark as Complete"} </button>
       </div>
     )}
     </>
