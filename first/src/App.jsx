fdf9bef579438340ea1707280ae66e0cc6fd318c
Rahul <rahulrawat4060@gmail.com>
2025-01-19 21:24:32 +0530

    add Hooks component to fetch and display todo data

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import Header from './components/Header'
-
+import Hooks from './components/Hooks'
 function App() {
   
 
   return (
     <>
     <h2>Hi</h2>
-      <Header/>
-      <Header title="Rahul"/>
+      {/* <Header/>
+      <Header title="Rahul"/> */}
+      <Hooks/>
     </>
   )
 }
---
+++ first/src/components/Hooks.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+function Hooks() {
+  const [id, setId] = useState(1);
+  const [data, setData] = useState();
+
+  useEffect 
+    (() => {
+      fetch(`https://jsonplaceholder.typicode.com/todos${id}`)
+        .then((response) => response.json())
+        .then((data) => setData(data));
+    },
+    []);
+  return (
+    <>
+      <div>Todos</div>
+      <div>id:{id}</div>
+      <div>title:{data?.title}</div>
+      <div>completed:{data?.completed}</div>
+      <div>userId:{data?.userId}</div>
+      <button onClick={() => setId(id + 1)}>Increment</button>
+    </>
+  );
+}
+
+export default Hooks;
