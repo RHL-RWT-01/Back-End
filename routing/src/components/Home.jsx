75486fc04caede6786d37fd30af98c6a25106d1b
Rahul <rahulrawat4060@gmail.com>
2025-01-20 21:46:09 +0530

    style: enhance Home component layout and styling

--- routing/src/components/Home.jsx
+++ routing/src/components/Home.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+     <div style={{textAlign: 'center'
+        , marginTop: '100px'
+        , marginBottom: '100px'
+        , backgroundColor: '#f8f9fa'
+        , padding: '20px'
+        , borderRadius: '10px'
+        , boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)'
+        , width: '50%'
+        , margin: 'auto'
+     }}>
+        <h1>Home</h1>
+        <p>Welcome to the Home Page</p>
+        
+     </div>
+    </>
   )
 }
 
