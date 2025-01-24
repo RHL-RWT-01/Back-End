96d68c38dfe360cee63de01351a5d97d08bedd37
Rahul <rahulrawat4060@gmail.com>
2025-01-24 23:32:28 +0530

    styling with new profile section

--- tailwind-practice/src/App.jsx
+++ tailwind-practice/src/App.jsx
@@ -1,22 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import my from "./assets/my.jpg";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div class='flex justify-center item-center h-screen bg-pink-500 dark:bg-black text-white'> 
-      <div class='w-12 flex-1 '>Hi</div>
-      <div class='w-64 flex-1'>Rahul</div>
-      <div class='w-32 flex-1'>Rawat</div>
-      <div class='w-25 flex-1'>Hello</div>
-    </div>
-      
+      <div class="bg-black">
+        <div class="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+          <img
+            class="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0"
+            src={my}
+            alt=""
+          />
+          <div class="space-y-2 text-center sm:text-left">
+            <div class="space-y-0.5">
+              <p class="text-lg font-semibold text-black">Erin Lindford</p>
+              <p class="font-medium text-gray-500">Product Engineer</p>
+            </div>
+            <button class="border-purple-200 text-pink-600 hover:border-transparent hover:bg-pink-900 hover:text-black active:bg-purple-700 ...">
+              Message
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
